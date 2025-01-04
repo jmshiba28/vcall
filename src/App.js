3ccef880b94c550eee7db01a888f57ed67af93de
jmshiba28 <jmshiba28cse@gmail.com>
2025-01-04 18:41:31 +0530

    Using react js, i'm create a chat application with zegocloud host.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+// import {Routes,Route} from 'react-router-dom'
+// // import Home from './pages/home/home'
+// import Roompage from './pages/Room/index'
 
+// function App() {
+//   return (
+//     <Roompage/>
+//     // <Routes>
+//     //   {/* <Route path="/" element={<Home />}></Route> */}
+      
+//     //   <Route path="/room/1" element={<Roompage/>}></Route>
+
+//     // </Routes>
+   
+//   );
+// }
+
+// export default App;
+import {Routes,Route} from 'react-router-dom'
+import Home from './pages/home/home'
+import Roompage from './pages/Room/index'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/room/:roomid" element={<Roompage />}></Route>
+
+    </Routes>
+   
   );
 }
 
